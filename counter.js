1893382b327589cf48bc80bd47ff5e750e6591db
Sindre <sindre.boyum@gmail.com>
2016-11-05 16:06:09 +0100

    ADD: Counter.js. Increments the Google'd count by one for each Google Search page load.

---
+++ counter.js
@@ -0,0 +1,14 @@
+var url = window.location.href;
+if (url && url.includes('google') && url.includes('search')) {
+  var count = 0;
+  chrome.storage.sync.get('sb-google-count', function (googleCount) {
+    if (googleCount['sb-google-count']) {
+      count = +googleCount['sb-google-count'];
+    }
+
+    count++;
+    chrome.storage.sync.set({
+      'sb-google-count': count
+    }, function () {});
+  });
+}
