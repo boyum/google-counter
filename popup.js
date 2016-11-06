b46319e7e3c05de8d5ec1b2c80901d93a18df4fa
Sindre <sindre.boyum@gmail.com>
2016-11-06 18:16:11 +0100

    ADD: Reset functionality

--- popup.js
+++ popup.js
@@ -1,6 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
+  getCount();
+  initResetButton();
+
+});
+
+function getCount() {
   chrome.storage.sync.get('sb-google-count', function (googleCount) {
     var el = document.getElementById('count');
     count.innerText = googleCount['sb-google-count'] || 0;
   });
-});
+}
+
+function initResetButton() {
+  var button = document.getElementById('reset-button');
+  button.addEventListener('click', resetCounter);
+
+  function resetCounter() {
+
+    chrome.storage.sync.set({
+      'sb-google-count': 0
+    }, function () {});
+
+    // Reset badge by sending a message to eventpage.js
+    chrome.runtime.sendMessage({
+      count: 0
+    }, function () {});
+
+    location.reload();
+  }
+}
