3c4c4b2cbff6991cb4b09a99ae052b2c160e5735
Sindre Bøyum <sindre.boyum@gmail.com>
2017-04-27 16:57:15 +0200

    ADD: Fireworks! By combining the google-counter with Eddie Lin's https://codepen.io/yshlin/pen/ylDEk, the first easter egg is now brought to counters out there.

--- counter.js
+++ counter.js
@@ -5,16 +5,57 @@ if (url && url.includes('google') && url.includes('search')) {
     if (googleCount['sb-google-count']) {
       count = +googleCount['sb-google-count'];
     }
-
     count++;
     chrome.storage.sync.set({
       'sb-google-count': count
     }, function () {});
 
     sendMessage(count);
+    runEasterEgg(count);
   });
 }
 
+/**
+ * Stores a message in the cloud storage.
+ * @param {string} message 
+ */
 function sendMessage(message) {
   chrome.runtime.sendMessage({count: message}, function() {});
+}
+
+/**
+ * Runs easter eggs whenever the count reaches certain stages.
+ * @param {number} count 
+ */
+function runEasterEgg(count) {
+  if (count % 100 === 0) {
+      showFireworks();    
+  }
+}
+
+/**
+ * Creates fireworks and appends them to the DOM.
+ * (Fireworks made by Eddie Lin https://codepen.io/yshlin/pen/ylDEk).
+ */
+function showFireworks() {
+  var pyro = document.createElement('div');
+
+  pyro.classList.add('google-counter__pyro');
+
+  document.body.appendChild(pyro);
+
+
+  /** Hide fireworks when all animations are done playing. */
+  window.setTimeout(hideFireworks, 16 * 1000);
+}
+
+/**
+ * Removes the `div.pyro` element from the DOM.
+ */
+function hideFireworks() {
+  var pyro = document.querySelector('.google-counter__pyro');
+
+  if (pyro !== null) {
+    pyro.parentNode.removeChild(pyro);
+  }
 }
